e23bc05215cae7873a458b2a7ff005c3b32b7282
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2021-03-13 20:07:05 +0100

    finalizar primeira versão

--- main.js
+++ main.js
@@ -3,77 +3,114 @@ const { app, Tray, Menu, dialog, MenuItem } = require('electron');
 const Store = require('electron-store');
 const { spawn } = require('cross-spawn');
 
+const Sentry = require('@sentry/electron');
+const fixPath = require('fix-path');
+const fs = require('fs');
+
+
+fixPath();
+
+// Sentry.init({ dsn: "https://fee20a99f2fe44428e2f11c7654ec87a@o551235.ingest.sentry.io/5674477" });
+
+
 const schema = {
     projects: {
-        type: 'string'
-    }
+        type: 'string',
+    },
+};
+
+let mainTray = {};
+
+if (app.dock) {
+    app.dock.hide();
 }
 
 const store = new Store({ schema })
 
-app.whenReady().then(() => {
-    // store.clear();
-    appIcon = new Tray('assets/iconTemplate.png');
+function getLocale() {
+    const locale = app.getLocale();
+
+    switch (locale) {
+        case 'es-419' || 'es':
+            return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/es.json')));
+        case 'pt-BR' || 'pt-PT':
+            return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/pt.json')));
+        default:
+            return JSON.parse(fs.readFileSync(resolve(__dirname, 'locale/en.json')));
+    }
+}
+
+function render(tray = mainTray) {
     const storedProjects = store.get('projects');
     const projects = storedProjects ? JSON.parse(storedProjects) : [];
+    const locale = getLocale();
 
-    const items = projects.map((project) => {
-        return {
-            label: project.name,
-            click: () => {
-                spawn.sync('code', [project.path])
-            }
-        }
-    })
+    const items = projects.map(({ name, path }) => ({
+        label: name,
+        submenu: [{
+                label: locale.open,
+                click: () => {
+                    spawn('code', [path], { shell: true });
+                },
+            },
+            {
+                label: locale.remove,
+                click: () => {
+                    store.set('projects', JSON.stringify(projects.filter(item => item.path !== path)));
+                    render();
+                },
+            },
+        ],
+    }));
 
     const contextMenu = Menu.buildFromTemplate([{
-            type: 'separator'
-        }, {
-            label: 'Local',
-            submenu: [...items]
-        },
-        {
-            label: 'Remotos',
-            submenu: []
-        },
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Configuração'
-        },
-        {
-            label: 'Fechar',
+            label: locale.add,
             click: () => {
-                app.exit();
-            }
-        }
-    ])
+                const result = dialog.showOpenDialog({ properties: ['openDirectory'] });
 
-    contextMenu.insert(0, new MenuItem({
-        label: 'Adicionar novo Projecto',
-        click: () => {
-            const [path] = dialog.showOpenDialogSync({ properties: ['openDirectory'] })
-            const name = basename(path)
-            store.set('projects', JSON.stringify([...projects, { path, name }]))
+                if (!result) return;
 
-            const item = new MenuItem({
-                label: name,
-                click: () => {
-                    spawn.sync('code', [path])
+                const [path] = result;
+                const name = basename(path);
 
-                }
-            })
+                store.set(
+                    'projects',
+                    JSON.stringify([
+                        ...projects,
+                        {
+                            path,
+                            name,
+                        },
+                    ]),
+                );
 
-            contextMenu.append(item)
+                render();
+            },
+        },
+        {
+            type: 'separator',
+        },
+        ...items,
+        {
+            type: 'separator',
+        },
+        {
+            type: 'normal',
+            label: locale.close,
+            role: 'quit',
+            enabled: true,
+        },
+    ]);
 
-        }
-    }))
+    tray.setContextMenu(contextMenu);
 
-    // Make a change to the context menu
+    tray.on('click', tray.popUpContextMenu);
+}
 
-    // Call this again for Linux because we modified the context menu
 
-    appIcon.setContextMenu(contextMenu)
-        // store.clear();
+app.whenReady().then(() => {
+    // store.clear();
+    mainTray = new Tray('assets/iconTemplate.png');
+
+    render(mainTray);
 })
